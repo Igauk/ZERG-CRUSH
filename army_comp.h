126093e5035632cc24c67c8fdc08124f3d4e1955
Igauk <igauk@ualberta.ca>
2023-12-05 10:55:45 -0700

    Fix units being assigned multiple times to squadron

--- army_comp.h
+++ army_comp.h
@@ -150,6 +150,7 @@ public:
     }
 
     void assignSquadronMember(const sc2::Unit* unit) {
+        if (std::find(units.begin(), units.end(), unit) != units.end()) return;
         units.push_back(unit);
     }
 
@@ -311,7 +312,6 @@ public:
 
             if (!squadronsWithRoom.empty()) {
                 sc2::GetRandomEntry(squadronsWithRoom)->assignSquadronMember(unit);
-                return;
             } else if (unit->unit_type != sc2::UNIT_TYPEID::TERRAN_SCV && !armySquadrons.empty()) {
                 sc2::GetRandomEntry(armySquadrons)->assignSquadronMember(unit);
             } else if (unit->unit_type != sc2::UNIT_TYPEID::TERRAN_SCV && !squadronsForUnit.empty()) {

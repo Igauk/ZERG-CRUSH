7a7379e7640434a508051c2feb0716f24820623e
Igauk <igauk@ualberta.ca>
2023-12-06 09:29:10 -0700

    Remove commented out code

--- main.cpp
+++ main.cpp
@@ -11,20 +11,6 @@
 // LadderInterface allows the bot to be tested against the built-in AI or
 // played against other bots
 int main(int argc, char* argv[]) {
-	/*
-	sc2::Coordinator coordinator;
-    coordinator.LoadSettings(argc, argv);
-
-    coordinator.SetParticipants({
-        CreateParticipant(sc2::Race::Terran, new ZergCrush()),
-        CreateParticipant(sc2::Race::Terran, new MultiplayerBot())
-    });
-
-    coordinator.LaunchStarcraft();
-    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
-
-    while (coordinator.Update()) {}
-	*/
 	RunBot(argc, argv, new ZergCrush(), sc2::Race::Terran);
 	return 0;
 }
